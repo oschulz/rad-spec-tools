7e3371f152b5b2d52a9696eeae9d788c6862ef45
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2012-08-16 02:32:56 +0200

    Initial template project layout

---
+++ src/template-project-LinkDef.h
@@ -0,0 +1,11 @@
+#ifndef template_project_LinkDef_h
+#define template_project_LinkDef_h
+
+#ifdef __CINT__
+
+// HelloWorld.h
+#pragma link C++ class tmplns::HelloWorld-;
+
+#endif
+
+#endif // template_project_LinkDef_h
